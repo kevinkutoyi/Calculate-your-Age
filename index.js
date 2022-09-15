428f17a30a83549c04441ec43a834b1990a51cdb
kevinkutoyi <kevinkutoyi95@gmail.com>
2022-09-15 17:14:05 +0100

    second commit

--- index.js
+++ index.js
@@ -1,10 +1,19 @@
 // let myDate = document.getElementById("my-date");
-let age = document.getElementById("age");
+let years = document.getElementById("years");
+let months = document.getElementById("months");
 var date = new Date();
 // myDate.innerHTML = date.getFullYear();
 function myFunction() {
   var birthYear = prompt("Enter your birth year");
   if (birthYear != null) {
-    age.innerHTML = `You are ${date.getFullYear() - birthYear} years old`;
+    years.innerHTML = `You are ${date.getFullYear() - birthYear} years old`;
+  }
+}
+function yearFunction() {
+  var birthYear = prompt("Enter your birth year");
+  if (birthYear != null) {
+    months.innerHTML = `You are ${
+      (date.getFullYear() - birthYear) * 12
+    } months old`;
   }
 }
