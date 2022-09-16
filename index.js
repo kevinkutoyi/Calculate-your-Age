71ee1b7b50290411341f384db723cd87a7ef727f
kevinkutoyi <kevinkutoyi95@gmail.com>
2022-09-16 08:11:59 +0100

    fourth commit

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 // let myDate = document.getElementById("my-date");
 let years = document.getElementById("years");
 let months = document.getElementById("months");
+let weeks = document.getElementById("weeks");
 var date = new Date();
 // myDate.innerHTML = date.getFullYear();
 function myFunction() {
@@ -15,5 +16,13 @@ function yearFunction() {
     months.innerHTML = `You are ${
       (date.getFullYear() - birthYear) * 12
     } months old`;
+  }
+}
+function weeksFunction() {
+  var birthYear = prompt("Enter your birth year");
+  if (birthYear != null) {
+    weeks.innerHTML = `You are ${
+      (date.getFullYear() - birthYear) * 52
+    } weeks old`;
   }
 }
